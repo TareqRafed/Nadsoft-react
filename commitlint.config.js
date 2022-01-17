8e1c487130efe6a25b1b3b025f145647cdef8389
TareqRafed <tariq@nadsoft.net>
2022-01-17 04:35:39 +0200

    chore: NADSOFT-0001 added link for commits convention

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
 module.exports = { extends: ['@commitlint/config-conventional'] }
+
+// Please read:
+// https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional#type-enum
+// to understand what commits are allowed
